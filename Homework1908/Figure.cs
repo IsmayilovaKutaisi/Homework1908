87fc1a8e9763448c559ab4ae625fc4cc7ac06863
IsmayilovaKutaisi <kutaisni@code.edu.az>
2024-08-24 04:01:48 +0400

    Add files via upload

---
+++ Homework1908/Figure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Homework1908
+{
+	abstract class Figure
+	{
+		public abstract double CalcArea();
+	}
+
+	class Square:Figure
+	{
+		public double Side { get; set; }
+        public override double CalcArea()
+        {
+            if(Side<=0)
+			{
+				Console.WriteLine("Side cannot be negative");
+			}
+			return Side * Side;
+        }
+    }
+
+	class Rectangular:Figure
+	{
+		public double Width { get; set; }
+		public double Length { get; set; }
+        public override double CalcArea()
+        {
+            if(Width<=0&&Length<=0)
+			{
+				Console.WriteLine("Width and Length cannot be megative");
+			}
+			return Width * Length;
+        }
+    }
+}
+
---
+++ Homework1908/Program.cs
@@ -0,0 +1,48 @@
+﻿using Homework1908;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1.Square");
+            Console.WriteLine("2.Rectangular");
+            Console.WriteLine("0.Quit");
+            Console.WriteLine("Enter your choice");
+
+            double choice = int.Parse.Console.ReadLine();
+            switch (choice)
+            {
+                    case1:
+
+            Console.WriteLine("Enter the value of the side of the square");
+            double side = double.Parse(Console.ReadLine());
+            var square = new Square { Side = side };
+            Console.WriteLine($"Square area:{square.CalcArea}");
+        case2:
+
+            Console.WriteLine("Enter the value of the width of the Rectangular");
+            double width = double.Parse(Console.ReadLine());
+            var rectangular = new Rectangular(width = width);
+            Console.WriteLine("Enter the value of the length of the rectangular");
+            double.length = double.Parse(Console.ReadLine());
+            var rectangular = new Rectangular(length = length);
+            new Rectangular { width = width, Length = length };
+            Console.WriteLine($"Rectangular area:{rectangular.CalcArea()}");
+            break;
+
+        case0:
+
+            return;
+
+            default:
+                    Console.WriteLine("Wrong choice! Please make the right choice.");
+            break;
+
+        } } }
+
+       
+  
+}
+
